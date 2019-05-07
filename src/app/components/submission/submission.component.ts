bdfc3deb403f7f15dee989c3eb57d6bb2364e437
81100877 <81100877@corp.root>
2019-05-07 17:52:05 +0800

    initial commit

---
+++ src/app/components/submission/submission.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submission',
+  templateUrl: './submission.component.html',
+  styleUrls: ['./submission.component.less']
+})
+export class SubmissionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  submitForm():void{
+
+  }
+
+}
